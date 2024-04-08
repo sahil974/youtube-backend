1ca64b86e34a05fbd541c60c3a5bd05510b7bea4
Sahil <sahifindmyphone@gmail.com>
2024-04-08 18:29:52 +0530

    backend template setup complete

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+
+const UploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath)
+            return null
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+
+        // file has been uploaded 
+        console.log("File is uploaded on cloudinary", response.url);
+
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the file from the server as the operation has failed
+        return null
+    }
+}
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
